56166ee0aa0515fbaba8b2b0329c9a2f5d253437
Roger <rmartinez5926.rm@gmail.com>
2020-07-01 12:42:54 -0400

    set up switch that will display todos with differnet status

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,7 +7,8 @@ class TodoList extends React.Component {
 
     /* Set up initial state */
     state = {
-        todos: []
+        todos: [],
+        displayed: "all"
     };
 
     /* create function that adds to the todos array */
@@ -20,11 +21,12 @@ class TodoList extends React.Component {
     /* create function to mark todos as complete */
     toggleStatus = id => {
         this.setState({
-            todos: this.state.todos.map(todo =>{
-                if(todo.id === id){
+            todos: this.state.todos.map(todo => {
+                /*check for the todo that needs to be changed to complete */
+                if (todo.id === id) {
                     return {
                         text: todo.text,
-                        id:todo.id,
+                        id: todo.id,
                         complete: !todo.complete
                     };
                 } else {
@@ -35,13 +37,30 @@ class TodoList extends React.Component {
     };
 
     render() {
+        let todos = [];
+        /* check which todos need to be displayed */
+        switch (this.state.displayed) {
+            case "all":
+                todos = this.state.todos;
+                break;
+            case "active":
+                todos = this.state.todos.filter(todo => !todo.complete)
+                break;
+            case "completed":
+                todos = this.state.todos.filter( todo => todo.complete)
+                break;
+            default:
+                console.log("error processing content to display.")
+                break;
+        }
+
         return (
             <div>
                 <TodoForm onSubmit={this.addTodo} />
                 {/* display number of active todos */}
                 <div>Todos Left: {this.state.todos.filter(todo => !todo.complete).length}</div>
-                {/* map over todos in the state to display all to page */}
-                {this.state.todos.map(todo => { return <Todo key={todo.id} toggleStatus={() => this.toggleStatus(todo.id)} todo={todo} />})}
+                {/* map over todos in the state to display to page */}
+                {todos.map(todo => { return <Todo key={todo.id} toggleStatus={() => this.toggleStatus(todo.id)} todo={todo} /> })}
             </div>
         )
     }
