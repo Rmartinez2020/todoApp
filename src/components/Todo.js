ef1403d2dd6e9e9814eeef2132c360d6cb6efe1a
Roger <rmartinez5926.rm@gmail.com>
2020-06-30 20:30:18 -0400

    set up main component to hold all todos in state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,25 @@
+// === Dependencies ===
 import React from 'react';
+import TodoForm from './TodoForm';
 
-class Todo extends React.Component{
-
+class Todo extends React.Component {
+    /* Set up initial state */
+    state = {
+        todos: []
+    };
+    /* create function that adds to the todos array */
+    addTodo = todo => {
+        this.setState({
+            todos: [todo, ...this.state.todos]
+        });
+    }
+    render() {
+        return (
+            <div>
+                <TodoForm onSubmit={this.addTodo} />
+            </div>
+        )
+    }
 }
 
 export default Todo;
