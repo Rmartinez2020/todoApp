967538429216042c55639b04b041aa7e110b6ae7
Roger <rmartinez5926.rm@gmail.com>
2020-07-02 09:32:48 -0400

    Updates

---
+++ precache-manifest.3c7af27e6894525a9c07a36e9a83c3c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf50976f45e5272724539d9701a6732",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "f29bb576264529cd9167",
+    "url": "/todoApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5624c84f16eb7b668a7b",
+    "url": "/todoApp/static/js/2.87ffe4ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.87ffe4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29bb576264529cd9167",
+    "url": "/todoApp/static/js/main.83088718.chunk.js"
+  },
+  {
+    "revision": "176b111f644b8b8c0526",
+    "url": "/todoApp/static/js/runtime-main.1e560017.js"
+  }
+]);
