54f9fd99925889653d712309acbdbcd0fb6aa3b1
Roger <rmartinez5926.rm@gmail.com>
2020-07-02 08:57:11 -0400

    add a button to complete a todo instead of clicking text

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,9 +9,9 @@ function Todo(props) {
                     textDecoration: props.todo.complete ? "line-through" : ""
                 }}
                 className="float-left"
-                onClick={props.toggleStatus}
             >{props.todo.text}</div>
             <button className="btn btn-danger ml-2 float-right" onClick={props.delete}>X</button>
+            <button className="btn float-right" onClick={props.toggleStatus}><i class="far fa-check-square fa-2x"></i></button>
         </li>
     )
 }
